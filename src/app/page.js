8d7e63b847da3d10d5defc75adfb7a994e51276e
Sharang Gupta <sharang.gupta@sharanggupta.dev>
2024-11-22 00:29:24 +0000

    Added Head component

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,24 @@ import ProductCardSection from "./components/ProductCardsSection/ProductCardSect
 import ReviewSection from "./components/Reviews/Reviews";
 import ContactForm from "./components/ContactForm/ContactForm";
 import Footer from "./components/Footer/Footer";
+import Head from 'next/head';
+
 
 export default function Home() {
   return (
     <div className="main-margin bg-[#f7f7f7] ">
+      <Head>
+        <title>Thiasil | Individually oxy-fired laboratory glassware</title>
+        <meta
+          name="description"
+          content="Manufacturers of individually oxy-fired laboratory glassware including crucibles, lids, basin"
+          key="desc"
+        />
+        <meta 
+            name="keywords" 
+              content="crucible, lid, quartz, silica, gooch crucible, muffle stand, volatile matter, circular capsule, boats combustion, triangles on nichrome wire" 
+        />
+      </Head>
       <Navbar />
       <HeroSection />
       <HiTechSection />
